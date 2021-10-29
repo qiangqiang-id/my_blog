6aca8bfcac83bbef853ef3595da8a6dd69efdfe5
qiangqiang <qiangq24@outlook.com>
2021-10-29 17:14:22 +0800

    fix : 错别字

--- blogs/.vuepress/siderbar.js
+++ blogs/.vuepress/siderbar.js
@@ -1,9 +1,9 @@
 const { createSideBarConfig } = require('./util')
-const JAVASCRIPT_PATH = '/blogs/git'
+const JAVASCRIPT_PATH = '/blogs/editor-picture'
 
 module.exports = {
   [JAVASCRIPT_PATH]: [
-    createSideBarConfig('git-基础', JAVASCRIPT_PATH),
+    createSideBarConfig('js小技巧', JAVASCRIPT_PATH),
   ],
   
 }
