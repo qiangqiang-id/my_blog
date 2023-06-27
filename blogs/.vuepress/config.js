accb4b25dbd15a86f326fe6001ff51d4af38afcb
qiangqiang-id <59910163+qiangqiang-id@users.noreply.github.com>
2023-06-27 15:49:17 +0800

    fix: 🐛 修改文件的base 路径

--- blogs/.vuepress/config.js
+++ blogs/.vuepress/config.js
@@ -1,42 +1,39 @@
-
 // const sidebar = require("./siderbar.js");
-module.exports =  {
+module.exports = {
+  base: '/my_blog',
   title: '小强的博客',
-  description: "站在巨人的肩上",
-  dest: "public",
+  description: '站在巨人的肩上',
+  dest: 'public',
   head: [
+    ['link', { rel: 'icon', href: '/img/favicon.png' }],
     [
-      'link',
-      { rel:"icon", href:'/img/favicon.png' }
-    ],
-    [
-      "meta",
+      'meta',
       {
-        name: "viewport",
-        content: "width=device-width,initial-scale=1,user-scalable=no",
+        name: 'viewport',
+        content: 'width=device-width,initial-scale=1,user-scalable=no',
       },
-    ]
+    ],
   ],
   theme: 'reco',
   themeConfig: {
     authorAvatar: '/img/favicon.png',
-    subSidebar: "auto",
+    subSidebar: 'auto',
     // valineConfig: {
     //   appId: "h6i7vXvVEK37fFFwb1DtMJ6W-gzGzoHsz",
     //   appKey: "cYI3hypnGAI00CPuzhNfeY8Q",
     // },
-    
+
     nav: [
-      { 
-        text: '首页' , 
-        link: '/', 
-        icon: 'reco-home'
+      {
+        text: '首页',
+        link: '/',
+        icon: 'reco-home',
       },
-      { 
+      {
         text: '时间轴',
-        link: '/timeline/', 
-        icon: 'reco-date' 
-      }, 
+        link: '/timeline/',
+        icon: 'reco-date',
+      },
     ],
     // friendLink: [
     //   {
@@ -57,41 +54,44 @@ module.exports =  {
     //    '/',
     //    ['/editor-picture','hello world']
     //   ],
-     type: "blog",
-     blogConfig: {
+    type: 'blog',
+    blogConfig: {
       category: {
         location: 2,
-        text: "目录索引",
+        text: '目录索引',
       },
       tag: {
         location: 3,
-        text: "标签索引",
+        text: '标签索引',
       },
     },
     search: true,
     searchMaxSuggestions: 10,
-    author: "xiaoqiang",
-    startYear: "2021",
+    author: 'xiaoqiang',
+    startYear: '2021',
     record: '小强',
   },
   markdown: {
     lineNumbers: true,
   },
   plugins: [
-   [
-      //鼠标点击特效 先安装在配置， 
-      "cursor-effects",
+    [
+      //鼠标点击特效 先安装在配置，
+      'cursor-effects',
       {
-        size: 3,                    // size of the particle, default: 2
-        shape: ['circle'],  // shape of the particle, default: 'star'
-        zIndex: 999999999           // z-index property of the canvas, default: 999999999
-      }
+        size: 3, // size of the particle, default: 2
+        shape: ['circle'], // shape of the particle, default: 'star'
+        zIndex: 999999999, // z-index property of the canvas, default: 999999999
+      },
     ],
-    ["vuepress-plugin-nuggets-style-copy", {
-      copyText: "复制代码",  //vuepress复制粘贴提示插件P 
-      tip: {
-        content: "复制成功!"
-      }
-    }],
+    [
+      'vuepress-plugin-nuggets-style-copy',
+      {
+        copyText: '复制代码', //vuepress复制粘贴提示插件P
+        tip: {
+          content: '复制成功!',
+        },
+      },
+    ],
   ],
 }
